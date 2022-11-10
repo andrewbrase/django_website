cdbce4365e88caae72b09c6d700ff5684eb5e0ba
Brase <brasea@wwt.com>
2022-11-09 20:25:21 -0600

    added docstrings to functions in view

--- basic_app/views.py
+++ basic_app/views.py
@@ -15,15 +15,19 @@ def index(request):
     return render(request, 'basic_app/index.html')
 
 def home(request):
+    '''home page'''
     return render(request, 'basic_app/home.html')
 
 def register(request):
+    '''register page, registered is set to false until form is submitted and verified'''
     registered = False
  
+    # if they submit the form w/ POST
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
         profile_form = UserProfileInfoForm(data=request.POST)
 
+        # verify that the forms are valid and save to database
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
@@ -32,14 +36,17 @@ def register(request):
             profile = profile_form.save(commit=False)
             profile.user = user
 
+            # save profile pic if user submitted one
             if 'profile_pic' in request.FILES:
                 profile.profile_pic = request.FILES['profile_pic']
 
             profile.save()
         
+            # registed is set to true, thank you is displayed (see html tag)
             registered = True
             
         else:
+            # form has errors
             print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
@@ -52,7 +59,7 @@ def register(request):
     })
 
 def signin(request):
-    # user has filled out and submitted login info
+    # user has filled out and submitted sign in info
     if request.method == 'POST':
         usern = request.POST.get('username')
         # you are using .get('username) on this:
@@ -77,13 +84,15 @@ def signin(request):
     else:
         return render(request, 'basic_app/index.html', {})
 
+# make sure someone is logged in to see this view!
+# do this with decorator
 @login_required
 def signout(request):
-    # make sure someone is logged in to see this view!
-    # do this with decorator
+    '''only if user is signed in, sign them out'''
     logout(request)
     return HttpResponseRedirect(reverse('index'))
 
 @login_required
 def contact(request):
+    '''contact page'''
     return render(request, 'basic_app/contact.html')
